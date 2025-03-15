e0a9a81173515710f6271616793fb1a52014baca
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-03-14 22:06:26 -0300

    commit

---
+++ Aula04/Aula04.java
@@ -0,0 +1,10 @@
+public class Aula04 {
+    public static void main(String[] args) {
+        Lapiseira l1 = new Lapiseira();
+        l1.setModelo("Bic cristal");
+        l1.setPonta(0.5f);
+        l1.status();
+        
+    }
+}
+ 
